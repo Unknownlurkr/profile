d445b8c690b1d2fdb7993c573f6867113251dcc6
Unknownlurkr <b.samantha.samuels@gmail.com>
2020-04-10 01:30:32 -0400

    added some code comments

--- promises.js
+++ promises.js
@@ -5,13 +5,15 @@ Brittany Samuels-200404362
 
 //grabbing the image from my git hub repo
 let ref = 'https://unknownlurkr.github.io/funky-products/chad.png'; //load to github 
+//json from github repo 
 let info = 'https://unknownlurkr.github.io/funky-products/text.json';
+//text file reference 
 let txt = 'https://unknownlurkr.github.io/funky-products/chad.txt';
-let p = document.querySelector('p');
+
 
 //Using a blob to fetch the image source
 //display a photo of the profile 
-
+//then createObjectUrl so we can reference the img tag and the blob has a reference
     fetch(ref).then(response =>{
         console.log(response);
         return response.blob();
@@ -21,15 +23,17 @@ let p = document.querySelector('p');
         document.getElementById('itm').src = URL.createObjectURL(blob);
     });
 
+    //async function txtInfo 
     async function txtInfo() {
+        //await for the fetch api to grab the text file that is being reference from txt
         let response = await fetch(txt);
-        let responseText = await response.text();
+        let responseText = await response.text(); 
     
-        document.getElementById('result').innerHTML = responseText;
+        document.getElementById('result').innerHTML = responseText; //result is reference to the div element
     }
     
     (async() => {
-        await txtInfo();
+        await txtInfo(); //awaiting for txtInfo to be called once browser has loaded the page
     })();
 /*grabing the text file created to upload the text content of the p tags within the html 
 fetch(txt).then(response =>{
